09a096fc0f1f1ed95d23235aa3504ff7005cc6b0
Nicholas <nicholasvoltani@gmail.com>
2024-12-26 16:30:20 -0300

    Quartz sync: Dec 26, 2024, 4:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Nicholas Funari Voltani",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
